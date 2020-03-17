94e1b4dfd4f72a7987d6fdfbbe094f33bd2df979
lvlichael72 <mahdi.moradi72@gmail.com>
2020-03-17 23:09:54 +0330

    Setting up Vuetify

--- Front/web/src/views/Home.vue
+++ Front/web/src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <h1>This is Home</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, id rerum. Quaerat aperiam numquam pariatur eligendi odio cupiditate architecto. Aspernatur explicabo dicta voluptatibus exercitationem, beatae deserunt qui dignissimos repellat aperiam?</p>
+    <p class="pink lighten-4 red--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sint molestias sunt dolore ad eligendi dicta cumque fugit mollitia, aliquid, id odit repudiandae natus fuga modi inventore incidunt quis. Ea.</p>
+    <h1 class="display-4">Massive Display</h1>
   </div>
 </template>
 
